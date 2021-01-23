73e224d8fb18ceb2396a629f9a01141b6cd56229
Dhruv Holkar <69317595+evandrula@users.noreply.github.com>
2021-01-23 23:29:54 +0530

    Added methods selectionSort()

--- ArraySorter.java
+++ ArraySorter.java
@@ -1,23 +1,35 @@
 public class ArraySorter {
     //ElementSwitcher
-    public static int[] switchElements(int[] inputArray,int a, int b){
+    protected static void switchElements(int[] inputArray, int a, int b){
         int d = inputArray[b];
         inputArray[b] = inputArray[a];
         inputArray[a] = d;
-        return inputArray;
     }
 
     //Bubble Sort
     public static int[] bubbleSort(int[] inputArray) {
-        int[] outputArray;
         for(int i = 0; i< inputArray.length; i++){
             for(int j = 0; j < inputArray.length-i-1; j++){
-                if(inputArray[j]>inputArray[j+1]){
+                if(inputArray[j] > inputArray[j+1]){
                     switchElements(inputArray, j,j+1);
                 }
             }
         }
-        outputArray = inputArray;
-        return outputArray;
+        return inputArray;
+    }
+
+    //Selection Sort
+    public static int[] selectionSort(int[] inputArray){
+        int min;
+        for (int i = 0; i < inputArray.length-1; i++){
+            min = i;
+            for (int j = i+1; j < inputArray.length; j++){
+                if(inputArray[j] < inputArray[min])
+                    min = j;
+            }
+            switchElements(inputArray, min, i);
+        }
+        return inputArray;
     }
 }
+
